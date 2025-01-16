96b9b87118f8b52c1eb2d97d64b55af4097cc45e
NavaFee <1457660144@qq.com>
2025-01-16 15:38:40 +0800

    feat(script): update script

--- scripts/mainnet_test.js
+++ scripts/mainnet_test.js
@@ -15,5 +15,17 @@ async function main() {
   console.log("feeRate:", feeRate.toString());
   const feeCollector = await dexRouter.getFeeCollector();
   console.log("feeCollector:", feeCollector);
+
+  // 设置手续费币种
+  // await dexRouter.setFeeTokens(
+  //   [process.env.BASE_USDC, process.env.BASE_USDT],
+  //   [true, true]
+  // );
+  checkIsFeeToken(dexRouter, process.env.BASE_USDC);
 }
 main();
+
+async function checkIsFeeToken(dexRouter, token) {
+  const isFeeToken = await dexRouter.checkIsFeeToken(token);
+  console.log(token, isFeeToken);
+}
